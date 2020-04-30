833494c1a0b28b01e4c9380d5a1ef6c1f73c7999
simonswiss <simon@simonswiss.com>
2020-04-30 10:15:20 +1000

    rounded corners and shadow, because.. why not

--- website/pages/index.js
+++ website/pages/index.js
@@ -5,7 +5,7 @@ import { useTheme } from "../tailwind-ds/core";
 export default function Homepage() {
   const { theme } = useTheme();
   return (
-    <div className="bg-primary py-4 px-8 w-full text-white">
+    <div className="bg-primary py-4 px-8 w-full text-white rounded-sm shadow-md">
       <h1 className="text-xl">Hello, {theme.replace("-", " ")}!</h1>
     </div>
   );
